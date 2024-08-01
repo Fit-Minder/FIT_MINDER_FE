8e7c8beb0ff42674560fb9984cfed39f76241e00
yumin22 <22joomin@naver.com>
2024-08-02 03:03:55 +0900

    [feat] 메인 nav bar 구현

--- fit-minder/src/components/main.js
+++ fit-minder/src/components/main.js
@@ -40,19 +40,30 @@ function Main() {
             .catch(error => console.error("Error fetching data:", error));
     }, []);
 
+    const [navVisible, setNavVisible] = useState(false);
 
+    const toggleNav = () => {
+        setNavVisible(!navVisible);
+    };
 
     return (
         <div className="Container">
             <div className="MainHeader">
                 <div className="Header">
                     <p>FITMINDER</p>
-                    <div className="Nav">
-                        <div></div>
-                        <div></div>
-                        <div></div>
+                    <div className={`Nav ${navVisible ? 'change' : ''}`}  onClick={toggleNav}>
+                        <div className="bar1"></div>
+                        <div className="bar2"></div>
+                        <div className="bar3"></div>
+                        
+                    </div>
+                    <div className={`NavContainer ${navVisible ? 'visible' : ''}`}>
+                        <a href="#">알림 설정</a>
+                        <a href="#">스트레칭</a>
+                        <a href="#">잔디 캘린더</a>  
                     </div>
                 </div>
+                
                 <button className="AlarmBtn">
                     <p>알람 설정 바로가기</p>
                     <img src={`${process.env.PUBLIC_URL}/alarm.png`} alt="" />
