714afcb20e8afc7a2f9bbb0553c9f59fb3f0960d
yumin22 <22joomin@naver.com>
2024-08-06 19:30:37 +0900

    [feat] fcm 구현

--- fit-minder/src/firebase.js
+++ fit-minder/src/firebase.js
@@ -1,30 +1,34 @@
-// src/firebase.js
-import { initializeApp } from 'firebase/app';
+import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+    apiKey: 'AIzaSyB4NJgBIXe_6oKZKi5Wt5pSyt9uPnSyni8',
+    authDomain: 'fitminderfcm.firebaseapp.com',
+    projectId: 'fitminderfcm',
+    storageBucket: 'fitminderfcm.appspot.com',
+    messagingSenderId: '851446458104',
+    appId: '1:851446458104:web:4238a3dec480ba25da7d0d',
+    measurementId: 'G-FZW7JTWGKN',
+  };
 
-const app = initializeApp(firebaseConfig);
+// Check if Firebase app is already initialized, if not initialize it
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
 const messaging = getMessaging(app);
 
 export const requestForToken = () => {
-  return getToken(messaging, { vapidKey: 'YOUR_PUBLIC_VAPID_KEY' }).then((currentToken) => {
-    if (currentToken) {
-      console.log('current token for client: ', currentToken);
-      // Perform any other neccessary action with the token
-    } else {
-      console.log('No registration token available. Request permission to generate one.');
-    }
-  }).catch((err) => {
-    console.log('An error occurred while retrieving token. ', err);
-  });
+  return getToken(messaging, { vapidKey: 'YOUR_PUBLIC_VAPID_KEY' })
+    .then((currentToken) => {
+      if (currentToken) {
+        console.log('current token for client: ', currentToken);
+        // Perform any other necessary action with the token
+      } else {
+        console.log('No registration token available. Request permission to generate one.');
+      }
+    })
+    .catch((err) => {
+      console.log('An error occurred while retrieving token. ', err);
+    });
 };
 
 export const onMessageListener = () =>
--- fit-minder/src/index.js
+++ fit-minder/src/index.js
@@ -6,15 +6,87 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'remixicon/fonts/remixicon.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import { createRoot } from 'react-dom/client';
+import {
+  getMessaging,
+  getToken,
+  onMessage,
+} from 'firebase/messaging';
+import { app } from './service/initFirebase';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/firebase-messaging-sw.js')
+      .then((registration) => {
+        console.log(
+          'Service Worker registration successful with scope: ',
+          registration.scope
+        );
+
+        navigator.serviceWorker.ready.then(() => {
+          const messaging = getMessaging(app);
+
+          getToken(messaging, {
+            vapidKey:
+              'BGr4GbsH8MB206HgLtGCkFSw0L0ZYE3Z-4KUGrYyW-PkCZHLIjU-5978gsTgr-qkDAYeu_Ue6Eh_710G7tvwu0k',
+          })
+            .then((currentToken) => {
+              if (currentToken) {
+                console.log(
+                  'Token retrieved: ',
+                  currentToken
+                );
+
+                // POST 요청 보내기
+                fetch(
+                  `https://like-fit.p-e.kr/api/v1/members/1?token=${currentToken}`,
+                    {
+                      method: 'POST',
+                     headers: {
+                      'Content-Type': 'application/json',
+                     },
+                     body: JSON.stringify({}),
+                   });
+              } else {
+                console.log(
+                  'No registration token available. Request permission to generate one.'
+                );
+              }
+            })
+            .catch((err) => {
+              console.log(
+                'An error occurred while retrieving token. ',
+                err
+              );
+            });
+
+          // 메시지 수신
+          onMessage(messaging, (payload) => {
+            console.log('Message received. ', payload);
+            // 여기에서 알림을 사용자 정의합니다
+          });
+        });
+      })
+      .catch((err) => {
+        console.log(
+          'Service Worker registration failed: ',
+          err
+        );
+      });
+  });
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
